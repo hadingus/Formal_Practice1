987fcf89571cce7d3cf26fb25a4e2ad07a1030a8
Eugene Tischenko <mr.hadingus@gmail.com>
2020-11-09 15:43:04 +0300

    added solution

---
+++ solution_src/solution.h
@@ -0,0 +1,39 @@
+//
+// Created by Евгений Тищенко on 09.11.2020.
+//
+
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+#include <optional>
+
+struct input {
+    std::string regular;
+    char symbol;
+    int symbCount;
+
+    input(const std::string &regular, char symbol, int count);
+};
+
+bool isLetter(char c);
+bool is_reg_char(char c);
+
+std::optional<input> getInput();
+std::pair<std::vector<bool>, std::vector<bool>> getTopTwice(std::stack<std::vector<bool>> &st);
+
+void push_letter(std::stack<std::vector<bool>> &reg_st, char symb, input &inp);
+std::vector<bool> mskAND(std::vector<bool> &msk1, std::vector<bool> &msk2);
+std::vector<bool> mskMUL(std::vector<bool> &msk1, std::vector<bool> &msk2);
+std::vector<bool> mskITER(std::vector<bool> &msk);
+
+void regAnd(std::stack<std::vector<bool>> &regStack);
+void regMul(std::stack<std::vector<bool>> &regStack);
+void regIter(std::stack<std::vector<bool>> &regStack);
+
+void solve();
+
+#endif
